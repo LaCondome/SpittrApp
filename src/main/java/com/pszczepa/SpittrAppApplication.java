64bc4c265c188d76b3781f830b2fa769bc8f7fb0
Piotr Szczepanek <piotr.szczepanek@gmail.com>
2016-09-14 22:56:27 +0200

    SpittrAp initial comment

---
+++ src/main/java/com/pszczepa/SpittrAppApplication.java
@@ -0,0 +1,12 @@
+package com.pszczepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpittrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpittrAppApplication.class, args);
+	}
+}
